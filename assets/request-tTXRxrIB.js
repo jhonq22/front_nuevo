fff9edac1aa80cc13a5004508dd7029a25a48f18
Jhon de Quesada <jhonqquesada@gmail.com>
2026-03-22 15:21:33 -0400

    qpatlogicoa

---
+++ assets/request-tTXRxrIB.js
@@ -0,0 +1,1 @@
+import{ad as s}from"./index-C89hzmgd.js";const r=s.create({baseURL:"http://citasmedicas.espromedbio.gob.ve",timeout:18e4});r.interceptors.request.use(e=>e,e=>(console.error("Error en la petición:",e),Promise.reject(e)));r.interceptors.response.use(e=>e.data,e=>(console.error("Error en la respuesta:",e),e.response?.data?.message,Promise.reject(e)));export{r as s};
